70d0566563428c0edd767f8219439b8af8c80df7
PartyMaker <d3m3@mail.ru>
2019-06-20 21:07:55 +0300

    Update ForKAS.cpp

--- ForKAS.cpp
+++ ForKAS.cpp
@@ -1,13 +1,18 @@
 ﻿#include "ForKAS.hpp"
 #include <iostream>
 #include <fstream>
+#include <thread>
 
 std::istream& operator>>(std::istream& str, CSVRow& data)
 {
 	data.readNextRow(str);
 	return str;
 }
-
+void do_somethink(product_set& product_set)
+{
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+	product_set.erase("720");
+}
 int main()
 {
 	product_set product_set;
@@ -20,9 +25,11 @@ int main()
 	auto id = product_set.find_id("10");
 	auto products = product_set.find_name("Acura");
 
+	std::thread t1(do_somethink, std::ref(product_set));
+	t1.detach();
 	for (auto it = products.first; it != products.second; it++)
 		std::cout << it->second->manufacturer + " " + it->second->id << std::endl;
-
+	
 	product_set.erase("11");
 	return 0;
 }
